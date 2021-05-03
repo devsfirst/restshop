ccf89b1f6ddc9020e7de20d261c07a3fd172553b
devsfirst <seokjh7172@naver.com>
2021-05-03 15:23:16 +0900

    init

---
+++ src/main/java/project/restshop/RestshopApplication.java
@@ -0,0 +1,13 @@
+package project.restshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestshopApplication.class, args);
+	}
+
+}
